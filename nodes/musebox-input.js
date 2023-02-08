5ecbcd08ca3c97c038b7693d44e6f16c0b60f65c
Enrico Giordano <enrico.giordano@makarenalabs.com>
2023-02-09 00:45:55 +0100

    added new operators

--- nodes/musebox-input.js
+++ nodes/musebox-input.js
@@ -269,34 +269,4 @@
     LiteGraph.registerNodeType("MuseBox Input/PC webcam", ImageWebcam);
 
 
-/******
- * SCALE
- */
-function Scale(){
-    this.addInput("image in", "frame,canvas,image");
-    this.addOutput("image out", "frame,canvas,image");
-    this.addProperty("width", "640");
-    this.addProperty("height", "480");
-}
-Scale.title = "Scale";
-Scale.desc = "Scale to fixed width and height";
-
-Scale.prototype.onConfigure = function(o) {
-};
-Scale.prototype.onExecute = function() {
-    var frame = this.getInputData(0);
-    if(frame){
-        var canvas = document.createElement('canvas'),
-        ctx = canvas.getContext("2d");
-        canvas.width = this.properties.width;
-        canvas.height = this.properties.height;
-        ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
-
-        this.setOutputData(0, canvas);
-    }
-};
-LiteGraph.registerNodeType("MuseBox Operators/Scale", Scale);
-
-
-
 })(this);
---
+++ nodes/musebox-operators.js
@@ -0,0 +1,57 @@
+//MuseBox input
+(function(global) {
+    var LiteGraph = global.LiteGraph;
+
+/******
+ * SCALE
+ */
+function Scale(){
+    this.addInput("image in", "frame,canvas,image");
+    this.addOutput("image out", "frame,canvas,image");
+    this.addProperty("width", "640");
+    this.addProperty("height", "480");
+}
+Scale.title = "Scale";
+Scale.desc = "Scale to fixed width and height";
+
+Scale.prototype.onConfigure = function(o) {
+};
+Scale.prototype.onExecute = function() {
+    var frame = this.getInputData(0);
+    if(frame){
+        var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext("2d");
+        canvas.width = this.properties.width;
+        canvas.height = this.properties.height;
+        ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
+
+        this.setOutputData(0, canvas);
+    }
+};
+LiteGraph.registerNodeType("MuseBox Operators/Scale", Scale);
+
+
+/******
+ * Count Bounding Boxes
+ */
+function CountBoundingBoxes(){
+    this.addInput("bounding box", "face bounding box, bounding box, bounding boxes");
+
+    this.addOutput("result", "number");
+}
+CountBoundingBoxes.title = "Count Bounding Boxes";
+CountBoundingBoxes.desc = "Scale to fixed width and height";
+
+CountBoundingBoxes.prototype.onExecute = function() {
+    var bbs = this.getInputData(0);
+    if(bbs){
+        this.setOutputData(0, bbs.length);
+    }
+    else {
+        this.setOutputData(0, 0);
+    }
+};
+LiteGraph.registerNodeType("MuseBox Operators/Count Bounding Boxes", CountBoundingBoxes);
+
+
+})(this);
